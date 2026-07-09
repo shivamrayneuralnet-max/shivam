9b207e6aa78841dbda46adf390eac86152b18b70
Shivam ray <shivamray.neuralnet@gmail.com>
2026-07-09 09:16:29 +0530

    Time: 0 ms (100%), Space: 12.2 MB (73.36%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                i--;
+                k--;
+              }
+        
+            else{                   //  if(nums1[i]<nums2[j]){
+                nums1[k]=nums2[j];
+                j--;
+                k--;
+            }
+        }
+        // remaining element 
+        if(j>=0) { // all the element if nums1 is filled
+          while(j>=0){
+            nums1[k]=nums2[j];
+            j--;
+            k--;
+          }
+         }
+
+        //  else condition is not require because first array is always required size array
+    }
+};
