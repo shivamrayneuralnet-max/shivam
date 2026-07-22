893cb44444d28d8cae2b468802a56e016d8dcf71
Shivam ray <shivamray.neuralnet@gmail.com>
2026-07-22 22:31:36 +0530

    Time: 0 ms (100%), Space: 26.2 MB (45.27%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        // previous greatest element 
+        int prev[n];
+        prev[0]=-1;
+        
+        int max=height[0];
+        for(int i=1;i<n;i++){
+            prev[i]=max;
+            if(max<height[i])  max=height[i];
+        }
+        // next greatest element ----> prev as a next
+        
+          prev[n-1]=-1;
+          max=height[n-1];
+          for(int i=n-2;i>=0;i--){
+           if(max<prev[i]) prev[i]=max;
+            if(max<height[i])  max=height[i];
+          }
+        //   // minimum array----> prev as minimum
+         
+        //   for(int i=0;i<n;i++){
+        //     prev[i]=min(prev[i],next[i]);
+        //   }
+          // calculating water
+          int water=0;
+          for(int i=1;i<n-1;i++){
+            if(height[i]<prev[i]){
+                water+=(prev[i]-height[i]);
+            }
+
+          }
+          return water;
+    }
+
+              
+};
