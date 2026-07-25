239f01cc3a71f41ac9ad9002e591b3c18fa9040a
Shivam ray <shivamray.neuralnet@gmail.com>
2026-07-25 07:33:59 +0530

    Time: 0 ms (100%), Space: 11.3 MB (41.95%) - LeetHub

---
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& grid) {
+        int rows=grid.size();
+        int col=grid[0].size();
+        // making the first columns all 1's
+        for(int i=0;i<rows;i++){
+           if(grid[i][0]==0){ // flip
+               for(int j=0;j<col;j++){
+                if(grid[i][j]==0) grid[i][j]=1;
+                else grid[i][j]=0;
+               }
+           }
+        }
+        // flip the columns where noz>noo
+        for(int j=0;j<col;j++){
+            int noz=0;
+            int noo=0;
+            for(int i=0;i<rows;i++){
+                
+                    if(grid[i][j]==0) noz++;
+                    else noo++;
+                
+            }
+            if(noz>noo){ // flip
+                for(int i=0;i<rows;i++){
+                        if(grid[i][j]==0) grid[i][j]=1;
+                else grid[i][j]=0;
+                }
+            }
+        }
+        // converting into decimal
+        int  sum=0;
+        for(int i=0;i<rows;i++){
+            int x=1;
+            for(int j=col-1;j>=0;j--){
+                sum+=grid[i][j]*x;
+                x*=2;
+            }
+        }
+        return sum;
+    }
+};
